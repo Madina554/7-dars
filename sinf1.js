9115007d811dba6aeadd60f751b403ccf79f235b
Madina <madinamuradova554@gmail.com>
2025-04-18 05:11:45 +0500

    save

---
+++ sinf1.js
@@ -0,0 +1,27 @@
+// let matn = "1234";
+// matn = matn.split("");
+// matn = matn.join("*");
+// console.log(matn);
+
+// let arr = [1, 2, 3, 4, 5];
+// let n = 4;
+// n = n % arr.length;
+// let end = arr.splice(arr.length - n);
+// console.log(end.concat(arr));
+
+
+
+// let arr = [1, 2, 3, 4, 4, 3, 23, 3, 3, 4, 4, 4, 4];
+// let obj = {};
+// arr.forEach((val) => {
+//     obj[val] = obj[val] != ? obj[val] + 1 : 1;
+// });
+// let maxCount = 0;
+// let k;
+// for (key in obj) {
+//     if (maxCount < obj[key]) {
+//         maxCount = obj[key];
+//         k = key;
+//     }
+// }
+// 81frf b b v c x
